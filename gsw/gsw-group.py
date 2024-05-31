1e3482eb9849bd2beb35780adf4cded92441ed0b
Aristeu Rozanski <arozansk@redhat.com>
2024-05-31 10:45:29 -0400

    gsw: introduce group
    Simple list implementation, but that's all that is needed right now
    
    Signed-off-by: Aristeu Rozanski <arozansk@redhat.com>

---
+++ gsw/gsw-group.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.  See http://www.gnu.org/copyleft/gpl.html for
+# the full text of the license.
+
+import getopt
+import sys
+import os
+
+# list ######
+def op_list(glab, opts, args):
+    for i in glab.groups.list():
+        print(i.name)
+
+def op_list_usage(f):
+    f.write("gsw %s list [-h|--help]\n\n" % MODULE_NAME)
+    f.write("-h|--help\t\tthis message\n")
+# list ######
+
+MODULE_NAME = "group"
+MODULE_OPERATIONS = { "list": op_list }
+MODULE_OPERATION_USAGE = { "list": op_list_usage }
+MODULE_OPERATION_SHORT_OPTIONS = { "list": "" }
+MODULE_OPERATION_LONG_OPTIONS = { "list": [] }
+MODULE_OPERATION_REQUIRED_ARGS = { "list": 0 }
+
+def list_operations(f):
+    for op in MODULE_OPERATIONS:
+        f.write("\t%s\n" % op)
+
+def gsw_module_main(config, glab, argv):
+    if len(argv) < 2:
+        print("%s <operation>" % MODULE_NAME)
+        print("Available operations:")
+        list_operations(sys.stdout)
+        sys.exit(0)
+    if argv[1] not in MODULE_OPERATIONS:
+        sys.stderr.write("Invalid operation: %s\n" % argv[1])
+        sys.stderr.write("Available operations:\n")
+        list_operations(sys.stderr)
+        sys.exit(2)
+
+    operation = argv[1]
+    short_options = MODULE_OPERATION_SHORT_OPTIONS[operation]
+    long_options = MODULE_OPERATION_LONG_OPTIONS[operation]
+    short_options += "h"
+    long_options.append("help")
+
+    try:
+        opts, args = getopt.getopt(argv[2:], short_options, long_options)
+    except getopt.GetoptError as err:
+        sys.stderr.write("%s\n" % err)
+        MODULE_OPERATION_USAGE[operation](sys.stderr)
+        sys.exit(2)
+    for o,v in opts:
+        if o == "-h" or o == "--help":
+            MODULE_OPERATION_USAGE[operation](sys.stdout)
+            sys.exit(0)
+    if len(args) < MODULE_OPERATION_REQUIRED_ARGS[operation]:
+        sys.stderr.write("Not enough arguments\n")
+        MODULE_OPERATION_USAGE[operation](sys.stderr)
+        sys.exit(2)
+
+    return MODULE_OPERATIONS[operation](glab, opts, args)
+
